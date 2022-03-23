6f991bbadff119eae6bb849cdac9c154af6de82c
pajith551@gmail.com <pajith551@gmail.com>
2022-03-23 22:37:04 +0530

    routs created

--- app.js
+++ app.js
@@ -16,19 +16,26 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
+
 app.get("/", function(req, res){
-  res.render("home")
-})
+  res.render("home", {startingContent: homeStartingContent});
+});
 
+app.get("/about", function(req, res){
+  res.render("about", {about: aboutContent});
+});
 
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
 
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
 
-
-
-
-
-
-
+app.post("/compose", function(req, res){
+  console.log(req.body.postTitle);
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
